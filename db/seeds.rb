951efb424f46d65618b5ed63852d2f0a6cc5be2a
Andre Menezes <djdedemenezes@gmail.com>
2021-09-22 21:11:48 -0300

    seed file using challenge data.json file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Parse JSON data 
+file = "#{File.dirname(__FILE__)}/../app/assets/challenge_briefing/starter-code/data.json"
+data = JSON.parse(File.read(file))
+
+puts 'Creating shop data'
+data.each do |element|
+  # Create category
+  category = Category.find_by(name: element['category'])
+  if category.nil?
+    category = Category.create!(name: element['category'])
+  end
+  product = Product.new(name: element['name'], price: element['price'], category: category, description: element['description'], features: element['features'])
+  product_pieces = element['includes'].map do |piece|
+    Piece.create!(name: piece['item'], amount: piece['quantity'])
+  end
+  product.pieces = product_pieces
+  product.save!
+
+  puts product.inspect
+end
+
+puts "#{Product.count} products created"
