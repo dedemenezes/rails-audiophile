7a0e115480058c4933c851cc5b58bcf9029dcd36
Andre Menezes <djdedemenezes@gmail.com>
2021-09-23 19:40:48 -0300

    seeding and attaching image to all products

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
   has_many :cart, through: :cart_products
   has_many :product_pieces
   has_many :pieces, through: :product_pieces
+  has_many_attached :photos
 
   validates :name, :price, :description, :features, presence: true
   validates :name, uniqueness: { case_sensitive: false }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+require 'pry-byebug'
+require 'open-uri'
+
+# START
+start_time = Time.now
+
 # Parse JSON data 
 file = "#{File.dirname(__FILE__)}/../app/assets/challenge_briefing/starter-code/data.json"
 data = JSON.parse(File.read(file))
@@ -20,3 +26,52 @@ data.each do |element|
 end
 
 puts "#{Product.count} products created"
+puts "***"
+puts "#{Time.now - start_time} seconds elapsed"
+puts "***"
+def cloudinary_file_name(an_string)
+  regex = /\w*-\w*\W*\d*.jpg$/
+  an_string.match(regex)[0][0...-4]
+end
+
+def attach_product_photos(devices_array, product)
+  puts "Uploading photos to cloudinary"
+  devices_array.each do |device, main_image_path|
+    # Upload photo to cloudinary
+    uploaded_photo = Cloudinary::Uploader.upload("app/assets/challenge_briefing/starter-code/#{main_image_path}", { public_id: device + '_' + cloudinary_file_name(main_image_path)})
+    # Open cloudinary file
+    file = URI.open(uploaded_photo['url'])
+    # attach photo to product
+    puts "attaching photo..."
+    product.photos.attach(io: file, filename: uploaded_photo['public_id'], content_type: 'image/jpg')
+  end
+  puts "photos attached"
+end
+
+
+data.each_with_index do |product, index|
+  # ./assets/product-yx1-earphones/desktop/image-product.jpg
+  # Get product
+  @product = Product.find(product['id'])
+  puts "Image seed for #{@product.name}"
+  # Get main image path for all devices
+  devices = product['image']
+  #attach photos
+  attach_product_photos(devices, @product)
+
+  puts '************************'
+  puts 'second round'
+  product['gallery'].each do |_key, devices|
+    attach_product_photos(devices, @product)
+  end
+    # devices.each do |device, img_path|
+    #   Cloudinary::Uploader.upload("app/assets/challenge_briefing/starter-code/#{img_path}", { public_id: device + '_' + cloudinary_file_name(img_path)})
+    # end
+end
+
+puts "photos uploaded"
+
+
+# Products.all.each_with_index do |product, index|
+#   product.photos.attach 
+# end
