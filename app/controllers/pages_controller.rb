2dc9bc14b4881c6f82167594f5ad3297df1450b2
Andre Menezes <djdedemenezes@gmail.com>
2021-09-25 16:22:21 -0300

    can access category product from main page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,13 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :top_products
 
   def home    
-    # unless user_signed_in?
-    #   cookies[:user] = 'temporary cart'
-    # end
-
-    # @cookies - cookies
     @categories = Category.all
+  end
+
+  private
+  def top_products
+    @top_products = { first: Product.find_by(price: Product.maximum('price')), second: Product.all.order('price').reverse.second }
   end
 end
