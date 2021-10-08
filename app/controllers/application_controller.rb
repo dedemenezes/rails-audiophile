76bb22b2dd752cea6aa3da9c80173ffb5f38970e
Andre Menezes <djdedemenezes@gmail.com>
2021-10-08 19:13:08 -0300

    setting cart at application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_cart
   include Pundit
 
   # Pundit: allowlist approach.
