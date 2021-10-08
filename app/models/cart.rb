044a9020b8a7895ae9bef757783aaf7cadbc5c24
Andre Menezes <djdedemenezes@gmail.com>
2021-10-08 19:14:28 -0300

    added method price_to_s on cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,6 +3,15 @@ class Cart < ApplicationRecord
   has_many :products, through: :cart_products
 
   def total_price
-    products.sum(:price)
+    price_to_s(products.sum(:price))
+  end
+  
+  def price_to_s(price)
+    if price.digits.size > 3
+      price_str = price.to_s
+      "$#{price_str[0...-3]}.#{price_str[-3..]},00"
+    else
+      "$#{price},00"
+    end
   end
 end
