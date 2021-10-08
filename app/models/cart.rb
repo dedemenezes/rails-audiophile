2afb458a400d672a1fa02489548e870f7207887f
Andre Menezes <djdedemenezes@gmail.com>
2021-10-08 16:51:24 -0300

    set cart to session

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,9 +1,16 @@
 class Cart < ApplicationRecord
-  belongs_to :user
-  has_many :cart_products
+  has_many :cart_products, dependent: :destroy
   has_many :products, through: :cart_products
+
 
   def total_price
     products.sum(:price)
+  end
+
+  def set_cart
+    @cart = Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create
+    session[:cart_id] = @cart.id
   end
 end
