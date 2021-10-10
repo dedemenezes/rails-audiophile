1b3c80aed87e45318677fd03a171966998d8fdc9
Andre Menezes <djdedemenezes@gmail.com>
2021-10-10 19:09:20 -0300

    login not needed to anything yet

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,5 @@
 class CartsController < ApplicationController
+  skip_before_action :authenticate_user!
   def show
     authorize @cart
   end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  skip_before_action :authenticate_user!
   before_action :set_products
 
   def index
