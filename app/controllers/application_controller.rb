141ba915b22683093626041fdf138239d800e2e1
andre menezes <djdedemenezes@gmail.com>
2021-11-13 14:52:10 -0300

    refactor banner for resposiveness

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   include CurrentCart
-  before_action :authenticate_user!, :set_cart, :all_categories
   include Pundit
+
+  before_action :authenticate_user!, :set_cart, :all_categories
 
   # Pundit: allowlist approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
@@ -20,9 +21,11 @@ class ApplicationController < ActionController::Base
   private
 
   def banner_image
-    if request.user_agent.match? /\b(Android|iPhone|iPad|Windows Phone|Opera Mobi|Kindle|BackBerry|PlayBook)\b/i
+    if request.user_agent.match? /\b(Android|iPhone|Windows Phone|Opera Mobi|Kindle|BackBerry|PlayBook)\b/i
       session[:device] = 'mobile'
       Cloudinary::Utils.cloudinary_url('mobile-banner-image')
+    elsif request.user_agent.match? /\b(iPad)\b/i
+      Cloudinary::Utils.cloudinary_url('tablet-banner-image')
     else
       session[:device] = 'desktop'
       Cloudinary::Utils.cloudinary_url('destktop-banner-image')
