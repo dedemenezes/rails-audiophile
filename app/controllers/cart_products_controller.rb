03d600eb13f5d6e1b3842281e92eb46e6b8dfba1
andre menezes <djdedemenezes@gmail.com>
2021-11-20 04:11:32 -0300

    we delete through ajax and update all amount that we display

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -12,7 +12,7 @@ class CartProductsController < ApplicationController
         format.js
         flash[:notice] = "Product added to cart"
       else
-        render :back
+        format.html { render :back }
         flash[:alert] = "Product NOTTTT added to cart"
       end
     end
@@ -21,15 +21,20 @@ class CartProductsController < ApplicationController
   def destroy
     @cart_product = CartProduct.find(params[:id])
     authorize @cart_product
-    if @cart_product.remove_product
-      if @cart.is_empty?
-        redirect_to root_path, notice: "Your cart is now empty."
+    respond_to do |format|
+      if @cart_product.remove_product
+        if @cart.is_empty?
+          format.html do
+            redirect_to root_path, notice: "Your cart is now empty."
+          end
+        else
+          format.js
+          flash[:notice] = "Product removed from cart"
+        end
       else
-        redirect_to cart_path(@cart), notice: "Product removed from cart"
+        flash[:alert] = "Product NOT removed from cart"
+        redirect_to cart_path(@cart_product.cart)
       end
-    else
-      flash[:alert] = "Product NOT removed from cart"
-      redirect_to cart_path(@cart_product.cart)
     end
   end
 end
--- app/javascript/components/animate_btn.js
+++ app/javascript/components/animate_btn.js
@@ -5,14 +5,12 @@ const animateOnClick = () => {
       if (event.button === 0){
         event.currentTarget.style.transition = '.2s'
         event.currentTarget.style.backgroundColor = 'black'
-        event.currentTarget.style.color = 'white'
       }
     })
     btn.addEventListener('mouseup', (event) => {
       if (event.button === 0) {
         event.currentTarget.style.transition = '.2s'
         event.currentTarget.style.backgroundColor = '#dc7d4ee3'
-        event.currentTarget.style.color = 'black'
       }
     })
   }
