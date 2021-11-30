a4533f4ec4a793a1f857e4ec88afaf0b6c879204
andre menezes <djdedemenezes@gmail.com>
2021-11-30 17:43:34 -0300

    fixed some tests

--- spec/models/cart_product_spec.rb
+++ spec/models/cart_product_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe CartProduct, type: :model do
     it { should belong_to(:cart) }
     it { should belong_to(:product) }
   end
+  describe '#add_product' do
+    before :each do
+      @cart = create(:cart)
+    end
+    context "when adding a product" do
+      it "add a product to the cart" do
+        @product = create :product
+        subject { CartProduct.add_product(@cart, @product) }
+        expect(subject).to be_a(CartProduct)
+        expect(subject).to respond_to(:quantity)
+        expect(subject.quantity).to eq(1)
+      end
+      it "add two different products" do
+        @product_one = create :product
+        @product_two = create :product
+        CartProduct.add_product(@cart, @product_one)
+        CartProduct.add_product(@cart, @product_two)
+        expect(@cart.cart_products.size).to eq(2)
+      end
+    end
+  end
 end
